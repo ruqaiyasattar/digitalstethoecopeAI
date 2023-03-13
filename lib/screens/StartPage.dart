0e8acad04b32e79a8eb8c7d43af8e0b1ae28164f
maqamylee0 <maqamylee0@gmail.com>
2023-03-13 11:28:20 +0300

    fixed startpage overflow

--- lib/screens/StartPage.dart
+++ lib/screens/StartPage.dart
@@ -9,44 +9,46 @@ class StartPage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: const Color(0xffF3F7FF),
-      body: Column(
-        children: [
-          const SizedBox(
-            height: 100,
-          ),
-          Padding(
-            padding: const EdgeInsets.only(left: 26,right: 26, top: 87),
-            child: Image.asset(
-                'assets/images/img.png',
-              height: 280,
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            const SizedBox(
+              height: 100,
             ),
-          ),
-          const Padding(
-            padding: EdgeInsets.only(left: 23, right: 23),
-            child: Text(
-                'mboathoscope',
-                 style: TextStyle(
-                 color: Color(0xff3D79FD),
-                 fontWeight: FontWeight.bold,
-                 fontSize: 50,
+            Padding(
+              padding: const EdgeInsets.only(left: 26,right: 26, top: 87),
+              child: Image.asset(
+                  'assets/images/img.png',
+                height: 280,
               ),
             ),
-          ),
-          const SizedBox(
-            height: 39,
-          ),
-          GestureDetector(
-            onTap: (){
-              Navigator.pushNamed(context,'/rolepage');
-            },
-            child: const Padding(
-              padding: EdgeInsets.only(bottom: 199),
-              child: CustomButton(
-                txt: 'Get Started',
+            const Padding(
+              padding: EdgeInsets.only(left: 23, right: 23),
+              child: Text(
+                  'mboathoscope',
+                   style: TextStyle(
+                   color: Color(0xff3D79FD),
+                   fontWeight: FontWeight.bold,
+                   fontSize: 50,
+                ),
               ),
             ),
-          )
-        ],
+            const SizedBox(
+              height: 39,
+            ),
+            GestureDetector(
+              onTap: (){
+                Navigator.pushNamed(context,'/rolepage');
+              },
+              child: const Padding(
+                padding: EdgeInsets.only(bottom: 199),
+                child: CustomButton(
+                  txt: 'Get Started',
+                ),
+              ),
+            )
+          ],
+        ),
       ),
     );
   }
