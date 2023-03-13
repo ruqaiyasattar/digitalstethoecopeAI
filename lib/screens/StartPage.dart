9955fcc9698f807da8d4cdde1cf1b0b08df1c016
dhruv1345 <21102156@mail.jiit.ac.in>
2023-03-13 20:47:24 +0530

    resolved screen overflow for starter page.

--- lib/screens/StartPage.dart
+++ lib/screens/StartPage.dart
@@ -7,48 +7,43 @@ class StartPage extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return SingleChildScrollView(
-      child: Scaffold(
-        backgroundColor: const Color(0xffF3F7FF),
-        body: Column(
-          children: [
-            const SizedBox(
-              height: 100,
+    return Scaffold(
+      backgroundColor: const Color(0xffF3F7FF),
+      body: Column(
+        children: [
+          Padding(
+            padding: const EdgeInsets.only(left: 26, right: 26, top: 87),
+            child: Image.asset(
+              'assets/images/img.png',
+              height: 280,
             ),
-            Padding(
-              padding: const EdgeInsets.only(left: 26, right: 26, top: 87),
-              child: Image.asset(
-                'assets/images/img.png',
-                height: 280,
+          ),
+          const Padding(
+            padding: EdgeInsets.only(left: 23, right: 23),
+            child: Text(
+              'mboathoscope',
+              style: TextStyle(
+                color: Color(0xff3D79FD),
+                fontWeight: FontWeight.bold,
+                fontSize: 45,
               ),
             ),
-            const Padding(
-              padding: EdgeInsets.only(left: 23, right: 23),
-              child: Text(
-                'mboathoscope',
-                style: TextStyle(
-                  color: Color(0xff3D79FD),
-                  fontWeight: FontWeight.bold,
-                  fontSize: 50,
-                ),
+          ),
+          const SizedBox(
+            height: 35,
+          ),
+          GestureDetector(
+            onTap: () {
+              Navigator.pushNamed(context, '/rolepage');
+            },
+            child: const Padding(
+              padding: EdgeInsets.only(bottom: 199),
+              child: CustomButton(
+                txt: 'Get Started',
               ),
             ),
-            const SizedBox(
-              height: 39,
-            ),
-            GestureDetector(
-              onTap: () {
-                Navigator.pushNamed(context, '/rolepage');
-              },
-              child: const Padding(
-                padding: EdgeInsets.only(bottom: 199),
-                child: CustomButton(
-                  txt: 'Get Started',
-                ),
-              ),
-            )
-          ],
-        ),
+          )
+        ],
       ),
     );
   }
